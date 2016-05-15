c0ea72c5832f5a65be925b152c5d77faefc6483c
elizlalala <eliz.marie.brown@gmail.com>
2016-05-15 16:41:09 -0700

    web server testing ground only

--- jquery.js
+++ jquery.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
 	var newGame = new ConnectFourGame();
 
+	var connection = new WebSocket('ws:172.16.51.254:8001');
+        connection.onmessage = function(event){
+            if (!newGame.checkWin()){
+			var column = $(this).attr('class').slice(-1);
+			var cell = newGame.playTurn(Number(column))
+			connection.send(column);
+			var row = Math.floor(cell/9)
+			$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
+		} else {
+			$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
+
+		}
+        }
+
 	$('.board-container div div').on('click', function(){
+
 		if (!newGame.checkWin()){
 			var column = $(this).attr('class').slice(-1);
 			var cell = newGame.playTurn(Number(column))
+			connection.send(column);
 			var row = Math.floor(cell/9)
 			$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
 		} else {
