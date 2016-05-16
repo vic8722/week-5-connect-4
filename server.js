97152ff0f29e06f571184cfcec158a4a547e7c8b
sweetkeys <shaun@shaunsweet.com>
2016-05-15 18:25:40 -0700

    make server side changes to handle locking out certain clients

---
+++ server.js
@@ -0,0 +1,27 @@
+var ws = require("nodejs-websocket")
+
+var clients = []
+var messages = []
+
+var server = ws.createServer(function(conn){
+    clients.push(conn);
+    
+    console.log("new connection");
+
+    conn.on("text", function(receivedMessage){
+        var column = receivedMessage
+    	
+        console.log("recieved")
+        clients.forEach(function(client){
+            client.sendText(column); 
+
+        })
+        conn.sendText(JSON.stringify({lock: true}));
+    })
+    conn.on("close", function(code, reason){
+        clients.splice(clients.indexOf(conn), 1);
+
+        console.log("connection closed for "+reason)
+    })
+}).listen(8001)
+
