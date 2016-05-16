f32a0a3f6ea8d01bd39d6cb5e45a8d1a233513df
vic8722 <victoria.trash@gmail.com>
2016-05-15 17:31:52 -0700

    Not sure

--- jquery.js
+++ jquery.js
@@ -1,32 +1,38 @@
 $(document).ready(function(){
 	var newGame = new ConnectFourGame();
+	// console.log("heyyyy");
 
 	var connection = new WebSocket('ws:172.16.51.254:8001');
         connection.onmessage = function(event){
-            if (!newGame.checkWin()){
-			var column = $(this).attr('class').slice(-1);
-			var cell = newGame.playTurn(Number(column))
-			connection.send(column);
-			var row = Math.floor(cell/9)
-			$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
-		} else {
-			$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
 
-		}
+					if (!newGame.checkWin()){
+        		var column = event.data;
+						// var column = $(this).attr('class').slice(-1);
+						var cell = newGame.playTurn(Number(column))
+						// connection.send(column);
+						var row = Math.floor(cell/9)
+						$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
+						if (newGame.checkWin()){
+							$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
+						}
+
+					}
+
+
         }
 
 	$('.board-container div div').on('click', function(){
 
-		if (!newGame.checkWin()){
+		// if (!newGame.checkWin()){
 			var column = $(this).attr('class').slice(-1);
-			var cell = newGame.playTurn(Number(column))
+			// var cell = newGame.playTurn(Number(column))
 			connection.send(column);
-			var row = Math.floor(cell/9)
-			$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
-		} else {
-			$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
+		// 	var row = Math.floor(cell/9)
+		// 	$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
+		// } else {
+		// 	$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
 
-		}
+		// }
 	}
 	)
 
