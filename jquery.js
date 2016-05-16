0351d06bd96cc73c8b80e86e6df3e0824497f8a4
vic8722 <victoria.trash@gmail.com>
2016-05-15 18:26:32 -0700

    Make it so you can't play after you just played a turn.

--- jquery.js
+++ jquery.js
@@ -1,30 +1,43 @@
 $(document).ready(function(){
 	var newGame = new ConnectFourGame();
+	var lock = false;
 	// console.log("heyyyy");
 
 	var connection = new WebSocket('ws:172.16.51.254:8001');
         connection.onmessage = function(event){
+        	console.log(event.data);
+        	receivedMessage = JSON.parse(event.data);
+        	console.log(receivedMessage);
+        	// console.log(receivedMessage.column);
 
-					if (!newGame.checkWin()){
-        		var column = event.data;
-						// var column = $(this).attr('class').slice(-1);
-						var cell = newGame.playTurn(Number(column))
-						// connection.send(column);
-						var row = Math.floor(cell/9)
-						$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
-						if (newGame.checkWin()){
-							$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
-						}
+        	if (receivedMessage.column){
+        						if (!newGame.checkWin()){
+        	        		var column = receivedMessage.column;
+        							// var column = $(this).attr('class').slice(-1);
+        							var cell = newGame.playTurn(Number(column))
+        							// connection.send(column);
+        							var row = Math.floor(cell/9)
+        							$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
+        							if (newGame.checkWin()){
+        								$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
+        							}
 
-					}
+        						}
+        						lock = false;
+        					}
+
+        	if (receivedMessage.lock){
+        		lock = true;
+        	}
 
 
         }
 
 	$('.board-container div div').on('click', function(){
-
-			var column = $(this).attr('class').slice(-1);
-			connection.send(column);
+			if (!lock){
+						var column = $(this).attr('class').slice(-1);
+						jsonObject = JSON.stringify({column: column});
+						connection.send(jsonObject);}
 	}
 	)
 
