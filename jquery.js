4d8cd83300bded27015e1159d58206da3609b609
sweetkeys <shaun@shaunsweet.com>
2016-05-19 11:24:08 -0700

    things

--- jquery.js
+++ jquery.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
         	// console.log(receivedMessage.column);
 
         	if (receivedMessage.column){
-        						if (!newGame.checkWin()){
-        	        		var column = receivedMessage.column;
+                  if (!newGame.checkWin()){
+                     var column = receivedMessage.column;
         							// var column = $(this).attr('class').slice(-1);
         							var cell = newGame.playTurn(Number(column))
         							// connection.send(column);
@@ -26,27 +26,27 @@ $(document).ready(function(){
         						lock = false;
         					}
 
-        	if (receivedMessage.lock){
-        		lock = true;
-        	}
+                                               if (receivedMessage.lock){
+                                                      lock = true;
+                                              }
 
-                if (receivedMessage.refresh) {
-                        alert("Your opponent left. :( New game initiated.");
-                        location.reload();
-                }
+                                              if (receivedMessage.refresh) {
+                                                alert("Your opponent left. :( New game initiated.");
+                                                location.reload();
+                                        }
 
 
-        }
+                                }
 
-	$('.board-container div div').on('click', function(){
-			if (!lock){
-						var column = $(this).attr('class').slice(-1);
-						jsonObject = JSON.stringify({column: column});
-						connection.send(jsonObject);}
-	}
-	)
+                                $('.board-container div div').on('click', function(){
+                                     if (!lock){
+                                          var column = $(this).attr('class').slice(-1);
+                                          jsonObject = JSON.stringify({column: column});
+                                          connection.send(jsonObject);}
+                                  }
+                                  )
 
-	$('#new-game-button').click(function() {
-				    location.reload();
-				});
-})
+                                $('#new-game-button').click(function() {
+                                        location.reload();
+                                });
+                        })
